ea9ce6a33fb4186500d2f6df6dc8bf82f954a92c
Dmitrij <grenko_d@protonmail.com>
2022-05-19 13:08:53 +0300

    Practice with _thread module

---
+++ System/Threads/thread-count-mutex.py
@@ -0,0 +1,23 @@
+"""
+Синхронизирует доступ к stdout: так как это общий глобальный объект, данные,
+которые выводятся из потоков выполнения, могут перемешиваться, если не синхронизировать
+ операции.
+"""
+
+import _thread as thread, time
+
+
+def counter(myId, count):  # эта функция выполнятся в потоках
+    for i in range(count):
+        time.sleep(1)  # имитировать работу
+        mutex.acquire()
+        print('[%s] => %s' % (myId, i))
+        mutex.release()
+
+
+mutex = thread.allocate_lock()  # создать объект блокировки
+for i in range(5):  # породить 5 потоков выполнения
+    thread.start_new_thread(counter, (i, 5))  # каждый поток выполняет 5 циклов
+
+time.sleep(6)
+print('Main thread exiting.')  # задержать выход из программы
---
+++ System/Threads/thread_count_wait1.py
@@ -0,0 +1,28 @@
+"""
+Использование мьютексов в родительском/главном потоке выполнения для определения
+момента завершения дочерних потоков, взамен time.sleep; блокирует stdout, чтобы
+избежать конфликтов при выводе;
+"""
+
+
+import _thread as thread
+
+
+stdoutmutex = thread.allocate_lock()
+exitmutexes = [thread.allocate_lock() for i in range(10)]
+
+
+def counter(myId, count):  # эта функция выполнятся в потоках
+    for i in range(count):
+        stdoutmutex.acquire()
+        print('[%s] => %s' % (myId, i))
+        stdoutmutex.release()
+    exitmutexes[myId].acquire()  # сигнал главному потоку
+
+
+for i in range(10):
+    thread.start_new_thread(counter, (i, 100))
+
+for mutex in exitmutexes:
+    while not mutex.locked(): pass
+print('Main thread exiting.')
