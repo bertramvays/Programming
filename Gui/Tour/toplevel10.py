21c3151327d2925bf3d423614ba8e81db2dcfb57
Dmitrij <grenko_d@protonmail.com>
2022-06-03 12:36:57 +0300

    Start using subwindows in tkinter

---
+++ Gui/Tour/toplevel10.py
@@ -0,0 +1,11 @@
+import sys
+from tkinter import Toplevel, Button, Label
+
+win1 = Toplevel()  # два независимых окна
+win2 = Toplevel()  # являющихся частью одного и того же процесса
+
+Button(win1, text='Spam', command=sys.exit).pack()
+Button(win2, text='SPAM', command=sys.exit).pack()
+
+Label(text='Popups').pack()  #  по умолчанию добавляется в корневок окно Tk()
+win1.mainloop()
