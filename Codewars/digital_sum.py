454a45d358f9be155cf21d37b0c518b1b719072c
Dmitrij <grenko_d@protonmail.com>
2022-07-21 17:03:13 +0300

    После долгого перерыва

--- Codewars/digital_sum.py
+++ Codewars/digital_sum.py
@@ -3,14 +3,14 @@ Digital root is the recursive sum of all the digits in a number.
 If that value has more than one digit, continue reducing in this way
 until a single-digit number is produced.
 """
-def digital_root(n):
-    lst = [int(i) for i in list(str(n))]
-    if len(lst) > 1:
-        result = sum(lst)
-        digital_root(result)
-    else:
-        result = lst[0]
-    return result
+    def digital_root(n):
+        lst = [int(i) for i in list(str(n))]
+        if len(lst) > 1:
+            result = sum(lst)
+            digital_root(result)
+        else:
+            result = lst[0]
+        return result
 
 
 
